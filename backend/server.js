ce8b2356df772f7f2147a42dcefe664afc7f3159
Boris <bxb454@case.edu>
2023-04-26 21:16:27 -0400

    Please work

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,11 @@ mongoose.connection.once('open', () => {
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Catch-all to serve the React app for any other route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
