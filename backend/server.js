b876809821d30a4905216d2a94663fe4ef813338
Boris <bxb454@case.edu>
2023-04-26 20:43:42 -0400

    Testing once more for functionality

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 // Catch-all to serve the React app for any other route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
